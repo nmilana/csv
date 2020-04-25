eee8a98c99a6242c14776f5c800ef01c590c0953
SerXP6 <cthutq58@gmail.com>
2020-04-25 18:13:26 +0300

    dogs

--- list-dogs.py
+++ list-dogs.py
@@ -10,12 +10,12 @@ if _in not in lst_in:
     print(f"Don't found '{_in}' folder")
     os.mkdir(f"in/{_in}")
     quit()
-print(f'{l} files {lst_in}')
+print(f'{l} files {lst_in} in "in" folder')
 
 
 lst_d = os.listdir(f"in/{_in}")
 l = len(lst_d)
-print(f'{l} files {lst_d}')
+print(f'{l} files {lst_d} in "in/list" folder')
 
 
 for fl in lst_d:
@@ -26,18 +26,29 @@ for fl in lst_d:
         print(f"Make '{fl[:-4]}' folder in out/{_in} folder")
         os.mkdir(f"out/{_in}/{fl[:-4]}")
 
-    dog = ''
+    dog1 = ''
     dog5 = ''
+    dog10 = ''
     i = 1
     for name in f:
-        dog += '@' + name[:-1] + ' '
-        # print(dog)
-        if not (i % 5):
-            dog5 += dog[:-1] + '\n'
-            dog = ''
+        print(name[:-1])
+        dog1 += '@' + name[:-1] + '\n'
+        dog5 += '@' + name[:-1] + (' ' if i % 5 else '\n')
+        dog10 += '@' + name[:-1] + (' ' if i % 10 else '\n')
         i += 1
-    dog5 += dog[:-1] + '\n'
+    dog5 = dog5[:-1] + '\n'
+    print(f'\n{dog5}')
+    dog10 = dog10[:-1] + '\n'
+    # print(dog10)
+
+    f = open(f"out/{_in}/{fl[:-4]}/dog1.txt", "w", encoding='utf-8')
+    f.write(f'{dog1}')
+    f.close()
 
     f = open(f"out/{_in}/{fl[:-4]}/dog5.txt", "w", encoding='utf-8')
     f.write(f'{dog5}')
+    f.close()
+
+    f = open(f"out/{_in}/{fl[:-4]}/dog10.txt", "w", encoding='utf-8')
+    f.write(f'{dog10}')
     f.close()
