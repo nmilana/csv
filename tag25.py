0535bc4ca854e262e3dca40106f7ee0caafeb5e3
SerXP6 <cthutq58@gmail.com>
2020-04-27 04:58:38 +0300

    tag25

---
+++ tag25.py
@@ -0,0 +1,25 @@
+import re
+
+_in = "mix"
+with open('./input/mix/tags.txt', 'rt', encoding='utf-8') as file:
+    tags_raw = file.read()
+
+list_tags = "#".join("".join(tags_raw.splitlines()).split('#')).split(' ')
+print(len(list_tags))
+
+i = 1
+tag25 = ''
+for item in list_tags:
+    tag25 += item + ' '
+    if not i % 25:
+        with open(f"out/{_in}/tag25_{str(int(i / 25)).zfill(2)}.txt", "w", encoding='utf-8') as file:
+            file.write(f'{tag25}')
+        tag25 = ''
+    i += 1
+
+print(tag25)
+
+if tag25 != '':
+    with open(f"out/{_in}/tag25_{str(int(i / 25) + 1).zfill(2)}.txt", "w", encoding='utf-8') as file:
+        file.write(f'{tag25}')   
+
